ea8c8ba0dd0cbe491fdba253cb641986a781d65c
MTFlacko <mateusfranc2@gmail.com>
2023-07-31 23:53:56 -0300

    Create ex22.py

---
+++ ex22.py
@@ -0,0 +1,7 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Analisando seu nome ...')
+print('Seu nome em maiuscula é {}'.format(nome.upper()))
+print('Seu nome em minusculas {}'.format(nome.lower()))
+print('Seu nome tem ao todo {}'.format(len(nome)-nome.count(' ')))
+print('Seu primeiro nome tem {} letras'.format(nome.find(" ")))
+
